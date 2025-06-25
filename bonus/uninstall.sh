63e6ba102fc9bb2ac23c5c995db5e54681845d53
Wolf Mardin <wolf.mardin@gmail.com>
2025-06-25 21:03:23 +0200

    knang

--- bonus/uninstall.sh
+++ bonus/uninstall.sh
@@ -1,7 +1,11 @@
 #helm uninstall gitlab -n gitlab
 #kubectl delete namespace gitlab
+
+
 rm -f /root/.ssh/id_rsa /root/.ssh/id_rsa.pub
 ssh-keygen -f '/root/.ssh/known_hosts' -R '[gitlab.localhost]:2226'
 
 rm -rf /tmp/repo_clone
 rm -rf /tmp/confs-copy
+
+argocd app delete wil-playground-bonus --yes
